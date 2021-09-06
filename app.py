8cc07685ec4ea9e10d311a6ac4f5fe5a4b538dc5
rajapramod <67217713+rajapramod@users.noreply.github.com>
2021-09-06 19:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 #------------------------- Imports -------------------------------
 import pandas as pd
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import plotly.express as px
 import plotly.graph_objects as go
